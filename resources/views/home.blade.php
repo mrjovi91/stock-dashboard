be876a81a038dca704f036de5faa0203f1f4b25e
Jovi Tan <tanyongjie.jovi91@gmail.com>
2021-10-03 17:55:31 +0800

    Finished mock layout for dashboard main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,46 @@
 @extends('layout.master')
 
 @section('content')
-@for($i = 1; $i < 4; $i++)
-    <div class="card">{{$i}}</div>
+<div class="row-center">
+@for($i = 1; $i <= 2; $i++)
+    <div class="card row-item">{{$i}}</div>
 @endfor
+    <div class="chart-card row-item">3</div>
+</div>
+
+<div class="row-center">
+    <table class="stocktable">
+        <thead>
+            <tr>
+                <th>Ticker</th>
+                <th>Name</th>
+                <th>Industry</th>
+                <th>Market Price</th>
+                <th>Average Paid</th>
+                <th>Position</th>
+                <th>% Gain/Loss</th>
+            <tr>
+        </thead>
+        <tbody>
+            @for($i = 1; $i <= 20; $i++)
+            @if($i %2 == 0)
+            <tr class="stocktable-row-even">
+            @else
+            <tr class="stocktable-row-odd">
+            @endif
+                <td>Ticker</td>
+                <td>Name</td>
+                <td>Industry</td>
+                <td>Market Price</td>
+                <td>Average Paid</td>
+                <td>Position</td>
+                <td>% Gain/Loss</td>
+            <tr>
+        @endfor
+        </tbody>
+    <table>
+
+
+
+
 @endsection
