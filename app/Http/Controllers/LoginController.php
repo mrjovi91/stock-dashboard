cd506aba988319e673475967218b78869d54a7f2
Jovi Tan <tanyongjie.jovi91@gmail.com>
2021-11-14 16:42:50 +0800

    Split login page view into subviews

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -11,7 +11,7 @@ class LoginController extends Controller
         if (Auth::check()){
             return redirect('/');
         }
-        return view('login');
+        return view('custom_auth.login');
     }
 
     public function authenticate(Request $request)
